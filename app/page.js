cbaadf879942f26dcb41e495d311944999e8982d
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-01-18 13:39:20 -0700

    last commit

--- app/page.js
+++ app/page.js
@@ -3,17 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
-        <div>
-        
-      </div>
-      <div>
-        <Link href="/week-2">
-        <text> week 2 Assignment</text>
-        </Link>
+      <div className="z-10 max-w-5xl w-full items-center justify-start font-mono text-sm lg:flex text-lg flex-col">
+        <h1 className="w-full">CPRG 306: Web Development 2 - Assignments</h1>
+        <div className="text-lg flex items-center justify-start w-full">
+        <Link href="/week-2"><text> week 2 Assignment</text></Link>
         </div>
-      </div>
+      </div>  
     </main>
   )
 }
