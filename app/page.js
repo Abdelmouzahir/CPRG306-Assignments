a7f2ba5823bd0f546049ffa9111c806fe601d9be
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-01-18 13:42:13 -0700

    h1 text

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-start font-mono text-sm lg:flex text-lg flex-col">
-        <h1 className="w-full">CPRG 306: Web Development 2 - Assignments</h1>
+        <h1 className="w-full text-lg">CPRG 306: Web Development 2 - Assignments</h1>
         <div className="text-lg flex items-center justify-start w-full">
         <Link href="/week-2"><text> week 2 Assignment</text></Link>
         </div>
