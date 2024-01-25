6835629aeef24fd50f49bda9faff3cacbc614eb4
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-01-24 18:56:28 -0700

    a

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
         <h1 className="w-full text-lg">CPRG 306: Web Development 2 - Assignments</h1>
         <div className="text-lg flex items-center justify-start w-full">
         <Link href="/week-2"><text> week 2 Assignment</text></Link>
-        <Link href="/week-3"><text> week 3 Assignment</text></Link>
         </div>
         <div className="text-lg flex items-center justify-start w-full">
         <Link href="/week-3"><text> week 3 Assignment</text></Link>
