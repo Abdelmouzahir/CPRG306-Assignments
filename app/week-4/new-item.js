6e3a542d1c03a3924d0566372d1b446469150194
abdel mouzahir <jalil.mouzahir@gmail.com>
2024-02-02 18:30:30 -0700

    week 4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,12 +1,63 @@
 "use client";
-
 import { useState } from "react";
 
-//name field
-const [name, setName] = useState("");
+export default function NewItem() {
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
 
-//quantity field
-const [quantity, setQuantity] = useState(1);
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-//category field
-const [category, setCategory] = useState("Produce");
+        let item = {
+            name: name,
+            quantity: quantity,
+            category: category
+        }
+
+        console.log(item);
+        alert("Added item: " + name + ", Quantity: " + quantity + ", Category: " + category);
+
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    };
+
+    return(
+        <main>
+            <div className="flex flex-col items-center justify-between font-bold text-4xl text-green-500 mt-16 mb-0">
+               <h1 >Produce Selector🥦🍍</h1>
+            </div>
+            <div className=" flex min-h-screen flex-col items-center justify-between p-24">
+            <form className=" p-2 m-2 text-black bg-orange-300 max-w-sm w-full rounded-md" onSubmit={handleSubmit}>
+                
+                <div className="m-2">
+                <input className="p-2 mb-2 rounded-md w-full" type="text" id="name" value={name} required placeholder="Item name" onChange={(event) => setName(event.target.value)} />
+                </div>
+                
+                <div className=" flex justify-between mt-2">
+
+                <input className="ml-2 p-1.5 w-16 rounded-md font-sans" type="number" id="quantity" value={quantity} min={1} max={99} required onChange={(event) => setQuantity(event.target.value)} />
+                
+                <select className="mr-2 p-1.5 w-36 rounded-md font-sans" id="category" value={category} onChange={(event) => setCategory(event.target.value)}>
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Frozen Foods">Frozen Foods</option>
+                    <option value="Canned Goods">Canned Goods</option>
+                    <option value="Dry Goods">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="Household">Household</option>
+                    <option value="Other">Other</option>
+                </select>
+
+                </div>
+                <br />
+                <button className="bg-green-500 w-full p-1.5 rounded-lg mb-2 text-white" type="submit">+</button>
+            </form>
+            </div>
+        </main>
+    );
+}
